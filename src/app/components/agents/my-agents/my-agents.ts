f43c7096e372e5ace6d3daf89ba14a65307a4807
Kevinxz98 <kevin.perea@infotegra.com>
2025-12-11 22:34:15 -0500

    ver agentes

--- src/app/components/agents/my-agents/my-agents.ts
+++ src/app/components/agents/my-agents/my-agents.ts
@@ -5,8 +5,9 @@ import {
   Columns,
   Config,
   DefaultConfig,
-  TableModule,
+  TableModule
 } from 'ngx-easy-table';
+
 import { FormsModule } from '@angular/forms';
 
 import { SpkDropdowns } from '../../../@spk/reusable-ui-elements/spk-dropdowns/spk-dropdowns';
@@ -17,12 +18,19 @@ import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import Swal from 'sweetalert2';
 import { ChangeDetectorRef } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-agents',
-  imports: [TableModule, FormsModule, SpkDropdowns, RouterModule, AsyncPipe, CommonModule],
+  imports: [
+    TableModule,
+    FormsModule,
+    SpkDropdowns,
+    RouterModule,
+    AsyncPipe,
+    CommonModule,
+  ],
   templateUrl: './my-agents.html',
   styleUrl: './my-agents.scss',
 })
@@ -30,7 +38,7 @@ export class MyAgents {
   constructor(
     private chatbotServices: ChatbotService,
     private cdr: ChangeDetectorRef,
-    private router: Router 
+    private router: Router
   ) {}
 
   public configuration!: Config;
@@ -45,10 +53,8 @@ export class MyAgents {
     this.configuration.checkboxes = true;
     this.columns = [
       { key: 'nombre', title: 'Nombre del Agente' },
-      { key: 'avatar', title: 'Avatar' },
       { key: 'nombreEmpresa', title: 'Empresa' },
-      { key: 'objetivoPrincipal', title: 'Objetivo' },
-      { key: 'estadoActivacion', title: 'Estado' },
+      { key: 'Status', title: 'Estado' },
       { key: 'Actions', title: 'Acciones' },
     ];
     this.loadMyAgents();
@@ -74,20 +80,45 @@ export class MyAgents {
     }
   }
 
+
   loadMyAgents() {
     this.agentes$ = this.chatbotServices.loadMyAgents().pipe(
       map((agentes: any[]) =>
         agentes.map((item) => ({
           ...item,
           nombre: item.nombre,
-          avatar: item.avatar ? `http://localhost:8000/storage/${item.avatar}` : null,
+          avatar: item.avatar
+            ? `http://localhost:8000/storage/${item.avatar}`
+            : null,
           nombreEmpresa: item.nombreEmpresa,
-          estadoActivacion: item.estadoActivacion,
+          Status: item.estadoActivacion,
           Date: item.created_at,
         }))
       )
     );
   }
 
-  
+  textColor(hexColor: string): string {
+    if (!hexColor) return '#000'; // Default: negro
+    hexColor = hexColor.replace('#', '');
+    const r = parseInt(hexColor.substring(0, 2), 16);
+    const g = parseInt(hexColor.substring(2, 4), 16);
+    const b = parseInt(hexColor.substring(4, 6), 16);
+    const luminancia = 0.299 * r + 0.587 * g + 0.114 * b;
+    return luminancia > 150 ? '#000' : '#fff';
+  }
+
+  statusLabel(status: string): { text: string, color: string } {
+    switch (status) {
+      case 'Activar':
+        return { text: 'Activo', color: 'text-success' };
+      case 'Solo guardar':
+        return { text: 'Guardado', color: 'text-warning' };
+      case 'Guardar como borrador':
+        return { text: 'Borrador', color: 'text-warning' };
+      default:
+        return { text: 'Inactivo', color: 'text-danger' };
+    }
+}
+
 }
